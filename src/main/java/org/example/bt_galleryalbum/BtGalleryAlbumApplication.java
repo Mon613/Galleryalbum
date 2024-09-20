e916fbcf2cdd6927c3f4da061e482fc9eaa232a7
Mon613 <nguyenha61314@gmail.com>
2024-09-20 22:41:00 +0700

    create project

---
+++ src/main/java/org/example/bt_galleryalbum/BtGalleryAlbumApplication.java
@@ -0,0 +1,13 @@
+package org.example.bt_galleryalbum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtGalleryAlbumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BtGalleryAlbumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/bt_galleryalbum/BtGalleryAlbumApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.bt_galleryalbum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtGalleryAlbumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
